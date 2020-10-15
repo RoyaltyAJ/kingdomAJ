91f839a8e3a6269d90475112a319d42aa7ffc3ff
RoyaltyAJ <44954672+RoyaltyAJ@users.noreply.github.com>
2020-10-15 18:33:13 -0400

    Add files via upload

---
+++ interfaz.js
@@ -0,0 +1,18 @@
+class interfaz{
+    agregarpersona() {
+
+    }
+    
+    eliminarpersona() {
+
+    }
+
+    mostarmensaje() {
+
+    }
+}
+
+document.getElementById('articulo-form').addEventListener('submit', function(){
+    alert('articulo subido')
+
+})
---
+++ persona.js
@@ -0,0 +1,10 @@
+class Persona{
+    constructor(nombre, apellido, carrera, titulo, comentario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.carrera = carrera;
+        this.titulo = titulo;
+        this.comentario = comentario;
+    }
+    
+}
